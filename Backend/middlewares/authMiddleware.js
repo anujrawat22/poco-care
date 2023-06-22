f4177553826c2aea429e2b07aefb1d85c2913cf2
anujrawat22 <anuj22rawat20@gmail.com>
2023-06-22 23:16:38 +0530

    authentication authorization done

---
+++ Backend/middlewares/authMiddleware.js
@@ -0,0 +1,42 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const authenticate = (req, res, next) => {
+  try {
+    const token = req.header?.authorization?.split(" ")[1];
+
+    const decoded = jwt.verify(token, process.env.privatekey);
+
+    if (decoded) {
+      req.userId = decoded.userId;
+      req.role = decoded.role;
+      next();
+    } else {
+      res.status(401).json({ msg: "Unauthorized" });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({ msg: "Unauthorized" });
+  }
+};
+
+const authorizeRoles = (roles) => {
+  return async (req, res, next) => {
+    try {
+      const role = req.role;
+
+      if (!roles.includes(role)) {
+        return res.status(403).json({ msg: "Forbidden" });
+      }
+      next();
+    } catch (error) {
+      return res.status(500).json({ message: "Server error" });
+    }
+  };
+};
+
+
+module.exports = {
+    authenticate,
+    authorizeRoles,
+  };
